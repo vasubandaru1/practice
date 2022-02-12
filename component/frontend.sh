833b036690d62f5352a2f1268bca97c128d7e648
vasu <bandaruvasu1@gmail.com>
2022-02-12 19:20:10 +0530

    frontend script

--- component/frontend.sh
+++ component/frontend.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+print() {
+  echo -e "\e[1m$1\e[0m"
+}
+
+print "Installing nginx"
 yum install nginx -y
 
+print "Enabling nginx"
 systemctl enable nginx
 
+print "Starting nginx"
 systemctl start nginx
-
+exit
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
 cd /usr/share/nginx/html
