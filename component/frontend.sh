b2207371e08099a6a13a6b07540471e8b0023ec2
vasu <bandaruvasu1@gmail.com>
2022-02-12 20:34:51 +0530

    frontend script

--- component/frontend.sh
+++ component/frontend.sh
@@ -4,27 +4,33 @@ print() {
   echo -e "\e[1m$1\e[0m"
   echo -e "\n\e[31m============================ $1 ======================\e[0m"
 }
+stat() {
+  if [ $1 -eq 0 ]; then
+    echo -e "\e[1;32mSUCESS\e[0m"
+    else
+      echo -e "\e[1;31mFAILURE\e[0m"
+      fi
+
+}
 LOG=/tmp/roboshop.log
 rm -f $LOG
 
 print "Installing nginx"
 yum install nginx -y &>>$LOG
-if [ $? -eq 0 ]; then
-  echo -e "\e[1;32mSUCESS\e[0m"
-  else
-    echo -e "\e[1;31mFAILURE\e[0m"
-    fi
-exit
+stat $?
+
 print "Enabling nginx"
 systemctl enable nginx &>>$LOG
+stat $?
 
 print "Starting nginx"
 systemctl start nginx &>>$LOG
+stat $?
 
 print "Downloading zipfile"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+stat $?
 exit
-
 cd /usr/share/nginx/html
 
 rm -rf *
