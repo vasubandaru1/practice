162f86e6f91351e39f9b51b9a3230cabb716455b
vasu <bandaruvasu1@gmail.com>
2022-02-12 19:30:51 +0530

    frontend script

--- component/frontend.sh
+++ component/frontend.sh
@@ -2,18 +2,23 @@
 
 print() {
   echo -e "\e[1m$1\e[0m"
+  echo -e "\n\e[31m============================ $1 ======================\e[0m"
 }
+LOG=/tmp/roboshop.log
+rm -rf $LOG
 
 print "Installing nginx"
-yum install nginx -y
+yum install nginx -y &>>$LOG
 
 print "Enabling nginx"
-systemctl enable nginx
+systemctl enable nginx &>>$LOG
 
 print "Starting nginx"
-systemctl start nginx
-exit
+systemctl start nginx &>>$LOG
+
+print "Downloading zipfile"
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+exit
 
 cd /usr/share/nginx/html
 
