62e54ff9c17f45f65ccf30958b055a362d886612
vasu <bandaruvasu1@gmail.com>
2022-02-13 08:47:48 +0530

    frontend script

--- component/frontend.sh
+++ component/frontend.sh
@@ -6,6 +6,24 @@ print "Installing nginx"
 yum install nginx -y &>>$LOG
 stat $?
 
+
+print "Downloading zipfile"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
+stat $?
+
+print "Remove old html pages"
+rm -rf /usr/share/nginx/html/* &>>$LOG
+
+print "Extract files"
+unzip -o -d /tmp /tmp/frontend.zip &>>$LOG
+
+print "Copy files to nginx path"
+mv /tmp/frontend-main/static/* /usr/share/nginx/html/. &>>$LOG
+stat $?
+
+print "Copy nginx conf files"
+mv tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
+
 print "Enabling nginx"
 systemctl enable nginx &>>$LOG
 stat $?
@@ -14,22 +32,3 @@ print "Starting nginx"
 systemctl start nginx &>>$LOG
 stat $?
 
-print "Downloading zipfile"
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
-stat $?
-
-cd /usr/share/nginx/html
-
-rm -rf *
-
-unzip /tmp/frontend.zip
-
- mv frontend-main/* .
-
- mv static/* .
-
- rm -rf frontend-master static README.md
-
- mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
-systemctl restart nginx
