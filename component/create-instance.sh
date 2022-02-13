b24552ff460500bfa074280020a50fa96646babd
vasu <bandaruvasu1@gmail.com>
2022-02-13 15:13:17 +0530

    Instance script file

--- component/create-instance.sh
+++ component/create-instance.sh
@@ -4,7 +4,7 @@ COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Re
 
 if [ $COUNT eq 0 ]; then
 
-aws ec2 run-instances --image-id ami-0d997c5f64a74852c --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+aws ec2 run-instances --image-id ami-0d997c5f64a74852c --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Values=$1}]" | jq
 
 else
   echo "Instance already exists"
