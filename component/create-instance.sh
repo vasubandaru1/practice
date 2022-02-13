92cd629b901b5f67b72596900ee9d2a5e08d42e8
vasu <bandaruvasu1@gmail.com>
2022-02-13 15:15:05 +0530

    Instance script file

--- component/create-instance.sh
+++ component/create-instance.sh
@@ -2,7 +2,7 @@
 
 COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
 
-if [ $COUNT eq 0 ]; then
+if [ $COUNT -eq 0 ]; then
 
 aws ec2 run-instances --image-id ami-0d997c5f64a74852c --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Values=$1}]" | jq
 
