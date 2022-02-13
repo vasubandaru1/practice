d9528893bbe29adcb82013194b0e625b3b7f6aeb
vasu <bandaruvasu1@gmail.com>
2022-02-13 21:58:42 +0530

    Instance script file

--- component/catalogue.sh
+++ component/catalogue.sh
@@ -22,7 +22,7 @@ curl -s -L -o /tmp/catalogue.zip  "https://github.com/roboshop-devops-project/ca
 stat $?
 
 print "Remove old content"
-rm -rf /home/ &>>$LOG
+rm -rf /home/roboshop/catalogue &>>$LOG
 stat $?
 
 print "Unzip a file"
