eb5ea810a05fb11baafe884781df9f56001c6bdc
vasu <bandaruvasu1@gmail.com>
2022-02-13 15:02:38 +0530

    Instance script file

--- component/create-instance.sh
+++ component/create-instance.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 
+COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
+
+if [ $COUNT eq 0 ]; then
+
 aws ec2 run-instances --image-id ami-0d997c5f64a74852c --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+
+else
+  echo "Instance already exists"
+
+  fi
+
+
+
+
+
