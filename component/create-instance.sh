27e4b71d7d8bb0e48ea064a1d2211c790700947d
vasu <bandaruvasu1@gmail.com>
2022-02-14 11:25:01 +0530

    Instance script file modification

--- component/create-instance.sh
+++ component/create-instance.sh
@@ -13,7 +13,7 @@ else
 
 IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | xargs )
 
-sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" shell sripting/record.json >/tmp/record.json
+sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" component/record.json >/tmp/record.json
 
 aws route53 change-resource-record-sets --hosted-zone-id Z09308302QPB0HR4UHDBS --change-batch file:///tmp/record.json | jq
 
