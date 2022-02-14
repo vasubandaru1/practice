ede45f9a2b54fe4e8750751df0c69ca0491a050e
vasu <bandaruvasu1@gmail.com>
2022-02-15 01:06:52 +0530

    Instance script file modification

---
+++ roboshop/component/redis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+source component/common.sh
+
+print "Install Redis repos"
+yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm -y &>>$LOG
+stat $?
+
+print "Enable redis repos"
+yum-config-manager --enable remi &>>$LOG
+stat $?
+
+print "Install Redis"
+yum install redis -y &>>$LOG
+stat $?
+
+print "Update Redis Listner"
+sed -i -e "s/127.0.0.1/0.0.0.0/" /etc/redis.conf /etc/redis/redis.conf &>>$LOG
+stat $?
+
+print "Restart redis services"
+systemctl enable redis && systemctl restart &>>$LOG
+stat $?
