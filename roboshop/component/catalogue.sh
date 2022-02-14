11d5874d29aa0bfc3922910693dcedc7692c62f4
vasu <bandaruvasu1@gmail.com>
2022-02-14 23:04:28 +0530

    Instance script file modification

--- roboshop/component/catalogue.sh
+++ roboshop/component/catalogue.sh
@@ -54,3 +54,13 @@ stat $?
 print "Start Catalogue services"
 systemctl daemon-reload && systemctl start catalogue && systemctl enable catalogue &>>$LOG
 stat $?
+
+print "Checking connected status of mongodb"
+STAT=$(curl -s localhost:8080/health | jq .mongo)
+echo status = $STAT
+if [ ""$STAT" == "true" ]; then
+  stat 0
+  else
+    stat 1
+ fi
+
