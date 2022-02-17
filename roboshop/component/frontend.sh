0c2c7a01d82eba9403436e914ff78d347f7f44b4
vasu <bandaruvasu1@gmail.com>
2022-02-17 12:06:04 +0530

    modification

--- roboshop/component/frontend.sh
+++ roboshop/component/frontend.sh
@@ -24,6 +24,11 @@ stat $?
 print "Copy nginx conf files"
 mv tmp/frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
 
+
+print "Update Nginx config file"
+sed -i -e "/catalogue/ s/localhost/catalogue.roboshop.internal/" -e "/cart/ s/localhost/cart.roboshop.internal/" -e "/shipping/ s/localhost/shipping.roboshop.internal/" -e "/payment/ s/localhost/payment.roboshop.internal/" -e "/user/ s/localhost/user.roboshop.internal/" /etc/nginx/default.d/roboshop.conf &>>$LOG
+
+
 print "Enabling nginx"
 systemctl enable nginx &>>$LOG
 stat $?
