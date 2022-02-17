4fd70b56d22d9a503e90401d01d2dfe36ace6789
vasu <bandaruvasu1@gmail.com>
2022-02-17 10:21:26 +0530

    modification

--- roboshop/component/rabbitmq.sh
+++ roboshop/component/rabbitmq.sh
@@ -8,7 +8,10 @@ COMPONENT_NAME=Rabbitmq
 COMPONENT=rabbitmq
 
 print "Install Erlang"
-yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>$LOG
+ yum list installed | grep erlang &>>$LOG
+
+ if [ $? -ne 0 ]; then
+   yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>>$LOG
 stat $?
 
 print "Setup YUM repositories "
