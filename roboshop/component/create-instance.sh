598514e2c4e547bb4d2a619d43d2fcecf96ac2ff
vasu <bandaruvasu1@gmail.com>
2022-02-18 07:51:47 +0530

    modification

--- roboshop/component/create-instance.sh
+++ roboshop/component/create-instance.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 #INSTANCE() {
-
-  COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
-
-  if [ $COUNT -eq 0 ]; then
-
-  aws ec2 run-instances --image-id ami-0d997c5f64a74852c --instance-type t2.micro --security-group-ids sg-0ac1d1ef4519d3ce6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
-
-  else
-    echo "Instance already exists"
-
-    fi
-
-  sleep 5
-
-  IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | xargs)
-
-  sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
-  aws route53 change-resource-record-sets --hosted-zone-id Z037744027VYZPYJX9NVX --change-batch file:///tmp/record.json
-
-}
+#
+#  COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | wc -l)
+#
+#  if [ $COUNT -eq 0 ]; then
+#
+#  aws ec2 run-instances --image-id ami-0d997c5f64a74852c --instance-type t2.micro --security-group-ids sg-0ac1d1ef4519d3ce6 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+#
+#  else
+#    echo "Instance already exists"
+#
+#    fi
+#
+#  sleep 5
+#
+#  IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null | xargs)
+#
+#  sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
+#  aws route53 change-resource-record-sets --hosted-zone-id Z037744027VYZPYJX9NVX --change-batch file:///tmp/record.json
+#
+#}
 
 if [ "$1" == "ALL" ]; then
 ALL=(catalogue mongodb shipping cart payment user mysql rabbitmq frontend redis)
